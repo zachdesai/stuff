bfdaa884b2af724bd8f984c6b3180c5a2426f0ac
zach d <zachdesai@gmail.com>
2022-12-01 18:38:47 -0500

    yes

---
+++ tLearningPython/helloWorld/main.py
@@ -0,0 +1,11 @@
+print("Hello World")
+print("Hello what is your name?")
+name = input("Enter your name:")
+print("HELLO " + name)
+print("what is your favorite colour")
+answer = input("enter your favourite coulour")
+
+if answer == "yellow":
+    print("i love that coulour")
+else:
+    print ("that is cool i like yellow")
