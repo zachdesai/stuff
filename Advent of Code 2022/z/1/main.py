872eb2a73e3967eee5735caf1a3b2e4a2ef79824
zach d <zachdesai@gmail.com>
2022-12-01 19:08:47 -0500

    day1

--- Advent of Code 2022/z/1/main.py
+++ Advent of Code 2022/z/1/main.py
@@ -1,2 +1,13 @@
-with open('input.txt','r') as file:
-    print(file)
+#! /usr/bin/python3
+
+with open('input.txt','r') as body:
+    arr = body.read().split('\n\n')
+    #print(arr)
+    list = []
+    for i in arr:
+        f = i.split('\n')
+        f = sum([int(g) for g in f])
+        list.append(f)
+    print(max(list))
+    list.sort(reverse=True)
+    print(sum(list[:3]))
